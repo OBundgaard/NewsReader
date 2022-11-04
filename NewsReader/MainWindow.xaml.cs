cb6ac3be0bbe4d5ca541a13792375c9458c8e11e
Kotka <kotkadkofficial@gmail.com>
2022-11-04 08:32:33 +0100

    Add project files.

---
+++ NewsReader/MainWindow.xaml.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NewsReader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        String serverName;    // The IP address
+        int serverPort = 119; // The port
+
+        // Convert an input String to bytes 
+        byte[] sendMessage = Encoding.UTF8.GetBytes("Hello Server, how are you today?\n");
+
+        TcpClient socket = null;
+        NetworkStream ns = null;
+        StreamReader reader = null; // Convinience
+
+        StreamReader sr;
+        StreamWriter sw;
+
+        string response;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Load the user data if any
+            UserData data = SaveSystem.LoadUserData();
+
+            if (data != null)
+            {
+                // We had data so set it into the respective textboxes
+                this.username.Text = data.Email;
+                this.password.Password = data.Password;
+                this.server.Text = data.Server;
+            }
+        }
+
+        private void loginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.username.Text == "" || this.password.Password == "" || this.server.Text == "")
+            {
+                MessageBox.Show("ERROR: No email, password or server inputted!");
+            }
+
+            // Saving the data
+            SaveSystem.SaveUserData(this.username.Text, this.password.Password, this.server.Text);
+
+            // Inserting values
+            this.serverName = this.server.Text;
+
+            // Deactivating whilst connecting
+            this.loginBtn.IsEnabled = false;
+            this.username.IsEnabled = false;
+            this.password.IsEnabled = false;
+            this.server.IsEnabled = false;
+
+            // Connect to the server
+            bool connectionSuccessful = ConnectToServer();
+
+            if (!connectionSuccessful)
+            {
+                // Setting the connection status
+                this.connectionStatus.Content = "Not Connected";
+                this.connectionStatus.Foreground = new SolidColorBrush(Colors.Red);
+
+                // Reactivating on fail
+                this.loginBtn.IsEnabled = true;
+                this.username.IsEnabled = true;
+                this.password.IsEnabled = true;
+                this.server.IsEnabled = true;
+
+                // Display error message
+                MessageBox.Show("ERROR: Can't connect to server");
+
+                return;
+            }
+
+            // Login to the server
+            bool loginSuccessful = LoginToServer(this.username.Text, this.password.Password);
+
+            if (!loginSuccessful)
+            {
+                // Setting the connection status
+                this.connectionStatus.Content = "Not Connected";
+                this.connectionStatus.Foreground = new SolidColorBrush(Colors.Red);
+
+                // Reactivating on fail
+                this.loginBtn.IsEnabled = true;
+                this.username.IsEnabled = true;
+                this.password.IsEnabled = true;
+                this.server.IsEnabled = true;
+
+                // Display error message
+                MessageBox.Show("ERROR: Incorrect username or password");
+
+                return;
+            }
+
+            // Connect and Login succeeded
+            // Setting the connection status
+            this.connectionStatus.Content = "Connected";
+            this.connectionStatus.Foreground = new SolidColorBrush(Colors.LightGreen);
+
+            // Add news groups to the list
+            AddGroupsToList();
+
+            return;
+        }
+
+        private void groupSelected(object sender, RoutedEventArgs e)
+        {
+            this.groupName.Text = this.newsgroups.SelectedItem.ToString();
+            this.groupArticles.Items.Clear();
+
+            this.sw.WriteLine($"group {this.groupName.Text}");
+            this.response = this.sr.ReadLine();
+            string[] parts = this.response.Split(' ');
+
+            // Get the article count and put it into the textbox
+            string articleCount = parts[1];
+            this.groupArticleCount.Content = $"{articleCount} Articles in group";
+
+            // Get articles in that group
+            GetGroupArticles(parts[2], parts[3]);
+
+            return;
+        }
+
+        private void articleSelected(object sender, RoutedEventArgs e)
+        {
+            
+            if (this.groupArticles.SelectedItem != null)
+            {
+                // We get the article ID
+                string articleID = this.groupArticles.SelectedItem.ToString();
+
+                // We execute the command
+                this.sw.WriteLine($"body {articleID}");
+
+                // Article Content
+                string articleContent = "";
+
+                // Get responses
+                for (int i = 0; i != 1000; i++)
+                {
+                    this.response = this.sr.ReadLine();
+
+                    if (i > 1)
+                    {
+                        articleContent += this.response + "\n";
+                    }
+
+                    if (this.response == ".")
+                    {
+                        break;
+                    }
+                }
+
+                this.selectedArticle.Text = articleContent;
+            }
+
+        }
+
+        private bool LoginToServer(string username, string password)
+        {
+            try
+            {
+                this.sr = new StreamReader(this.ns, System.Text.Encoding.Default);
+                this.sw = new StreamWriter(this.ns);
+                this.sw.AutoFlush = true;
+
+                sw.WriteLine($"authinfo user {username}");
+                this.response = sr.ReadLine();
+                Thread.Sleep(1000);
+
+                sw.WriteLine($"authinfo pass {password}");
+                this.response = sr.ReadLine();
+                Thread.Sleep(1000);
+
+                return this.response == "381 PASS required";
+            } catch
+            {
+                return false;
+            }
+        }
+
+        private bool ConnectToServer()
+        {
+
+            try
+            {
+                this.socket = new TcpClient(this.serverName, this.serverPort);
+                this.ns = this.socket.GetStream(); // Now we are connected
+                return true;
+            } catch
+            {
+                return false;
+            }
+
+        }
+
+        private List<string> GetNewsGroups()
+        {
+            // Making a new list for the groups
+            List<string> groups = new List<string>();
+
+            // Sending the command 'list' to the news server
+            this.sw.WriteLine("list");
+
+            // Skip responses that are unnecessary
+            SkipResponse(2);
+
+            for (int i = 0; i < 18629; i++)
+            {
+                this.response = this.sr.ReadLine();
+                groups.Add(response.Split(' ')[0]);
+            }
+
+            // Skip responses that are unnecessary
+            SkipResponse();
+
+            return groups;
+        }
+
+        private void GetGroupArticles(string firstArticleID, string lastArticleID)
+        {
+            for (int i = Int32.Parse(firstArticleID); i < Int32.Parse(lastArticleID)+1; i++)
+            {
+                this.groupArticles.Items.Add(i.ToString());
+            }
+        }
+
+        private void AddGroupsToList()
+        {
+            List<string> groups = GetNewsGroups();
+
+            foreach (string group in groups)
+            {
+                this.newsgroups.Items.Add(group);
+            }
+        }
+
+        // This method was used because I got multiple responses
+        // For every command I used
+        private void SkipResponse(int times = 1)
+        {
+            
+            if (times < 1)
+            {
+                return;
+            }
+
+            for (int i = 0; i < times; i++)
+            {
+                this.response = sr.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ NewsReader/SaveSystem.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace NewsReader
+{
+    public static class SaveSystem
+    {
+
+        public static void SaveUserData(string email, string password, string server)
+        {
+            // We create a new formatter
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            // We want to store our data in the project folder
+            string path = Environment.CurrentDirectory + "/data.nsld";
+
+            // We create a new file stream
+            FileStream stream = new FileStream(path, FileMode.Create);
+
+            // Now we store our data into one object
+            UserData data = new UserData(email, Encrypt(password), server);
+
+            // We serialize it
+            formatter.Serialize(stream, data);
+
+            // Then we close it
+            stream.Close();
+        }
+
+        public static UserData LoadUserData()
+        {
+            // We want to get our data from the project folder
+            string path = Environment.CurrentDirectory + "/data.nsld";
+
+            if (File.Exists(path))
+            {
+                // We create a new formatter
+                BinaryFormatter formatter = new BinaryFormatter();
+
+                // We create a new file stream
+                FileStream stream = new FileStream(path, FileMode.Open);
+
+                // We deserialize it as user data
+                UserData data = formatter.Deserialize(stream) as UserData;
+                data.Password = Decrypt(data.Password);
+
+                // Then we close it
+                stream.Close();
+
+                return data;
+            } else
+            {
+                return null;
+            }
+        }
+
+        private static string Encrypt(string password)
+        {
+            // We convert the password to bytes
+            byte[] bytes = Encoding.Default.GetBytes(password);
+
+            // Then we convert it to a string and removes "-"
+            // This gives us a HEX string
+            return BitConverter.ToString(bytes).Replace("-", "");
+        }
+
+        private static string Decrypt(string encryptedPassword)
+        {
+            // We create a new byte array
+            byte[] data = new byte[encryptedPassword.Length / 2];
+
+            // We loop through the byte array
+            for (int i = 0; i < data.Length; i++)
+            {
+                // We then convert substrings of the encrypted password to bytes
+                data[i] = Convert.ToByte(encryptedPassword.Substring(i * 2, 2), 16);
+            }
+
+            // Now we can now convert it to a string and then return the original password
+            return Encoding.ASCII.GetString(data);
+        }
+
+    }
+
+    [System.Serializable]
+    public class UserData
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Server { get; set; }
+
+        public UserData(string parsedEmail, string parsedPassword, string parsedServer)
+        {
+            this.Email = parsedEmail;
+            this.Password = parsedPassword;
+            this.Server = parsedServer;
+        }
+    }
+}
